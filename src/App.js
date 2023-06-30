223f7a701205d2972cba09eabfcc0778cba602c4
Anujsingh0706 <anujthakur0706@gmail.com>
2023-06-30 16:09:42 +0530

    changes in calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState("");
+
+  const handleButtonClick = (e) => {
+    setDisplay(display + e.target.value);
+  };
+
+  const calculate = () => {
+    try {
+      setDisplay(eval(display).toString());
+    } catch {
+      setDisplay("Error");
+    }
+  };
+
+  const clearDisplay = () => {
+    setDisplay("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input type="text" className="display" value={display} readOnly />
+      <div className="buttons">
+        <button value="1" onClick={handleButtonClick}>
+          1
+        </button>
+        <button value="2" onClick={handleButtonClick}>
+          2
+        </button>
+        <button value="3" onClick={handleButtonClick}>
+          3
+        </button>
+        <button value="+" onClick={handleButtonClick}>
+          +
+        </button>
+        <button value="4" onClick={handleButtonClick}>
+          4
+        </button>
+        <button value="5" onClick={handleButtonClick}>
+          5
+        </button>
+        <button value="6" onClick={handleButtonClick}>
+          6
+        </button>
+        <button value="-" onClick={handleButtonClick}>
+          -
+        </button>
+        <button value="7" onClick={handleButtonClick}>
+          7
+        </button>
+        <button value="8" onClick={handleButtonClick}>
+          8
+        </button>
+        <button value="9" onClick={handleButtonClick}>
+          9
+        </button>
+        <button value="*" onClick={handleButtonClick}>
+          *
+        </button>
+        <button value="0" onClick={handleButtonClick}>
+          0
+        </button>
+        <button onClick={calculate}>=</button>
+        <button value="/" onClick={handleButtonClick}>
+          /
+        </button>
+        <button onClick={clearDisplay}>Clear</button>
+      </div>
     </div>
   );
 }
